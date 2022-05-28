f94d3c32f29800817501c211c5321e9f26b2d883
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-05-28 17:46:33 -0400

    🚧 Add data props

--- components/Manga/Card.vue
+++ components/Manga/Card.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-3 py-2 border-t border-gray-300">
     <a href="#" class="hover:z-10">
       <img
-        src="https://mangadex.org/covers/6b958848-c885-4735-9201-12ee77abcb3c/101ada2e-2bc1-4247-be2f-72230d0a0346.jpg"
+        :src="cover"
         alt="temp"
         class="px-2 transition duration-150 ease-in-out transform hover:scale-125"
       />
@@ -10,20 +10,54 @@
     <div class="col-span-2 flex flex-col gap-2">
       <span class="border-b border-gray-300 truncate">
         <fa icon="book" :fixed-width="true" class="mr-1" />
-        <a href="#" class="font-bold text-blue-700">Hello World</a>
+        <a :href="`/manga/${mangaId}`" class="font-bold text-blue-700">
+          {{ mangaTitle }}
+        </a>
       </span>
       <span class="truncate">
         <fa icon="file" :fixed-width="true" class="mr-1" />
-        <a href="#" class="text-blue-700">Vol.2 Chapter 8.2 🇺🇸</a>
+        <a :href="`/chapter/${chapterId}`" class="text-blue-700">
+          <span v-if="volume">Vol.{{ `${volume} ` }}</span>
+          <span v-if="chapter">Ch.{{ `${chapter} ` }}</span>
+          <span v-if="title && !title.toLocaleLowerCase().includes('chapter')">
+            {{ title }}🇺
+          </span>
+        </a>
       </span>
       <span class="truncate">
         <fa icon="users" :fixed-width="true" class="mr-1" />
-        <a href="#" class="text-blue-700">KaBanks</a>
+        <a
+          :href="
+            scanlationGroup
+              ? `/group/${scanlationGroupId}`
+              : `/user/${uploaderId}`
+          "
+          class="text-blue-700"
+        >
+          {{ scanlationGroup || uploader }}
+        </a>
       </span>
       <span class="truncate">
         <fa icon="clock" :fixed-width="true" class="mr-1" />
-        <span>69 secs</span>
+        <span>{{ uploadedAt }}</span>
       </span>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  cover?: string;
+  volume?: string;
+  chapter?: string;
+  chapterId?: string;
+  mangaTitle?: string;
+  mangaId?: string;
+  scanlationGroup?: string;
+  scanlationGroupId?: string;
+  uploader?: string;
+  uploaderId?: string;
+  uploadedAt?: string;
+}>();
+</script>
