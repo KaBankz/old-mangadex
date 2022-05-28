ab054f7ee7e0a1cfa33a5c8755e103a13f229e58
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-05-28 01:02:53 -0400

    🐛 Fix dropdown zindex

--- components/Nav/Item.vue
+++ components/Nav/Item.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="inline">
-    <div class="relative inline-block text-left group">
+    <div class="relative inline-block text-left z-10 group">
       <button
         class="inline-flex justify-center w-full px-4 py-4 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-900"
         type="button"
