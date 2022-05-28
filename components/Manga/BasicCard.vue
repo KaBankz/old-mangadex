56575c5cababc3093b8c0041751e77809d2ca006
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-05-28 17:56:38 -0400

    🚧 Add data props

--- components/Manga/BasicCard.vue
+++ components/Manga/BasicCard.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-5 p-2 border-t border-gray-300 h-24">
     <a href="#" class="hover:z-10">
       <img
-        src="https://mangadex.org/covers/6b958848-c885-4735-9201-12ee77abcb3c/101ada2e-2bc1-4247-be2f-72230d0a0346.jpg"
+        :src="cover"
         alt="temp"
         class="px-2 h-20 transition duration-150 ease-in-out transform hover:scale-200"
       />
@@ -10,13 +10,23 @@
     <div class="col-span-4 flex flex-col gap-2">
       <span class="border-b border-b-gray-300 truncate">
         <fa icon="book" :fixed-width="true" class="mr-1" />
-        <a href="#" class="font-bold text-blue-700">Hello World</a>
+        <a :href="`/manga/${mangaId}`" class="font-bold text-blue-700">
+          {{ mangaTitle }}
+        </a>
       </span>
       <span>
         <div class="flex justify-between">
           <div class="truncate">
             <fa icon="file" :fixed-width="true" class="mr-1" />
-            <a href="#" class="text-blue-700">Vol.2 Chapter 8.2</a>
+            <a :href="`/chapter/${chapterId}`" class="text-blue-700">
+              <span v-if="volume">Vol.{{ `${volume} ` }}</span>
+              <span v-if="chapter">Ch.{{ `${chapter} ` }}</span>
+              <span
+                v-if="title && !title.toLocaleLowerCase().includes('chapter')"
+              >
+                {{ title }}🇺
+              </span>
+            </a>
           </div>
           <div class="truncate">
             <fa icon="eye" :fixed-width="true" class="mr-1" />
@@ -27,3 +37,20 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  cover?: string;
+  volume?: string;
+  chapter?: string;
+  chapterId?: string;
+  mangaTitle?: string;
+  mangaId?: string;
+  scanlationGroup?: string;
+  scanlationGroupId?: string;
+  uploader?: string;
+  uploaderId?: string;
+  uploadedAt?: string;
+}>();
+</script>
