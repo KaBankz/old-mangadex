8f094107e63e4d6a05a985bc2dd21d1551faa2bc
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-05-28 14:20:40 -0400

    🚧 Create basic cards

---
+++ components/Manga/BasicCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid grid-cols-5 p-2 border-t border-gray-300 h-24">
+    <a href="#">
+      <img
+        src="https://mangadex.org/covers/6b958848-c885-4735-9201-12ee77abcb3c/101ada2e-2bc1-4247-be2f-72230d0a0346.jpg"
+        alt="temp"
+        class="px-2 h-20 transition duration-150 ease-in-out transform hover:scale-200"
+      />
+    </a>
+    <div class="col-span-4 flex flex-col gap-2">
+      <span class="border-b border-b-gray-300">
+        <fa icon="book" :fixed-width="true" class="mr-1" />
+        <a href="#" class="font-bold text-blue-700">Hello World</a>
+      </span>
+      <span>
+        <div class="flex justify-between">
+          <div>
+            <fa icon="file" :fixed-width="true" class="mr-1" />
+            <a href="#" class="text-blue-700">Vol.2 Chapter 8.2</a>
+          </div>
+          <div>
+            <fa icon="eye" :fixed-width="true" class="mr-1" />
+            <span>69,420</span>
+          </div>
+        </div>
+      </span>
+    </div>
+  </div>
+</template>
