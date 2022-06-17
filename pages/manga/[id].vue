6dcd42d03d359c76e76e9f2eb2436f32278449ae
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-06-17 16:19:17 -0400

    🚧 Add placeholder manga pages

---
+++ pages/manga/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center font-bold text-4xl">
+    Welcome to
+    <pre class="inline-block">/manga/{{ $route.params.id }}</pre>
+    ! 🎉
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'main',
+});
+</script>
---
+++ pages/manga/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center font-bold text-4xl">
+    Welcome to
+    <pre class="inline-block">/manga</pre>
+    ! 🎉
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'main',
+});
+</script>
