3959b94e8b2738a2616b8668c372f95fddfe2e9d
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-06-17 18:22:53 -0400

    🔧 Add fontawesome brand icons to nuxt plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,8 @@ export default defineNuxtConfig({
       '@fortawesome/vue-fontawesome',
       '@fortawesome/fontawesome-svg-core',
       '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/free-brands-svg-icons',
     ],
   },
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
 });
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,9 +1,12 @@
 import { config, library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(fas);
-config.autoAddCss = true;
+library.add(fas, fab);
+
+// Disabled so nuxt can handle css and not fontawesome
+config.autoAddCss = false;
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('fa', FontAwesomeIcon);
